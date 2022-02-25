ecfbca1725ed71c9ed9c303050aeaa9d17b19c35
Dragonfly232 <RonjiaNellemann1@gmail.com>
2022-02-25 12:42:23 +0000

    adding structure to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
-console.log("connected");
+const canvas = document.getElemantById("pongGame");
+const context = canvas.getContext("2d");
+canvas.width = 700;
+canvas.height = 550;
 
+let scoreOne = 0;
+let scoreTwo = 0;
+
+class Element{
+    constructor(options){
+        this.x = options.x
+        this.y = options.y
+        this.x = options.x
+        this.y = options.y
+        this.x = options.x
+        this.y = options.y
+
+    }
+}
