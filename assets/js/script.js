9c5a0a234e9d564e959aea2e251e440930f250f2
Dragonfly232 <RonjiaNellemann1@gmail.com>
2022-02-26 13:22:41 +0000

    git

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElemantById("pongGame");
 const context = canvas.getContext("2d");
-canvas.width = 700;
-canvas.height = 550;
+canvas.width = 650;
+canvas.height = 400;
 
 let scoreOne = 0;
 let scoreTwo = 0;
@@ -20,6 +20,29 @@ class Element{
 }
 
 const PlayerOne = new Element({
-    
-    
-})
+    x: 10,
+    y: 200,
+    width: 80,
+    color: "#fff",
+    gravity: 2,
+    });
+    //second paddle
+
+    //ball
+    const ball = new Element({
+        x: 650 /2,
+        y: 400 /2
+
+    })
+    //Player one score text
+
+    //Player two score text
+
+    //draw elements
+    function drawElement(element) {
+
+
+    }
+
+    //detect collision
+
